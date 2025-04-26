c526e21db7748178b8e8210fa7fe917cbc92bab8
Vansh0204 <vanshyt425@gmail.com>
2025-04-27 01:22:05 +0530

    Add Storybook config

---
+++ .storybook/manager.ts
@@ -0,0 +1,13 @@
+import { addons } from '@storybook/manager-api';
+import { themes } from '@storybook/theming';
+
+addons.setConfig({
+  theme: themes.light,
+  toolbar: {
+    title: { hidden: false },
+    zoom: { hidden: false },
+    eject: { hidden: false },
+    copy: { hidden: false },
+    fullscreen: { hidden: false },
+  },
+}); 
